681d76e6028de27f2f72c28145ee45be115f02ff
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-02 15:08:57 +0100

    Intial commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,32 @@
+global using dotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CharacterController : ControllerBase 
+    {
+        private static List<Character> Characters  = new List<Character> {
+            new Character(),
+            new Character {Id=1, Name ="Mounya"}
+
+        };
+    
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(Characters);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(Characters.FirstOrDefault(c=> c.Id == id));;
+        }
+    }
+}
